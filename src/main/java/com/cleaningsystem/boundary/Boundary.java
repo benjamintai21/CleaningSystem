9f81ac3c02c8905d2372fcfba0d46115f944807d
Benjamin <benjamintai21@gmail.com>
2025-05-07 01:32:33 +0800

    done

--- src/main/java/com/cleaningsystem/boundary/Boundary.java
+++ src/main/java/com/cleaningsystem/boundary/Boundary.java
@@ -40,33 +40,27 @@ public class Boundary {
     public String processLogin(@ModelAttribute("loginForm") UserAccount user, Model model) {
         try {
             UserAccount loggedInUser = userAccountC.login(user.getUsername(), user.getPassword());
+
+            if (loggedInUser == null) {
+                model.addAttribute("error", "Invalid username or password");
+                return "login";
+            }
+
             UserProfile userProfile = userProfileC.getProfileById(loggedInUser.getProfileId());
             String profileName = userProfile.getProfileName();
-            // int profileId = loggedInUser.getProfileId();
-            // switch (profileId) {
-            //     case 1:
-            //         // return "PM_Home";
-            //         break;
-            //     case 2:
-            //         // return "User_Admin_Home";
-            //         break;
-            //     case 3:
-            //         // return "Home_Owner_Home";
-            //         break;
-            //     case 4:
-            //         // return "Cleaner_Home";
-            //         break;
-            // } 
+
             model.addAttribute("userAccountInfo", loggedInUser);
             model.addAttribute("profileName", profileName);
+
             return "user_account_info";
+
         } catch (Exception e) {
-            model.addAttribute("error", "Invalid username or password");
+            model.addAttribute("error", "An unexpected error occurred");
             return "login";
         }
-        
     }
 
+    //Create User aAccount
     @GetMapping("/CleanerUserCreation")
     public String showCleanerSignUpForm(Model model) {
         model.addAttribute("CleanerUserCreationForm", new UserAccount()); // Make sure this matches the form data type
@@ -76,54 +70,36 @@ public class Boundary {
     @PostMapping("/CleanerUserCreation")
     public String processCleanerSignUp(@ModelAttribute UserAccount user, Model model) {
         user.setProfileId(4);
-        boolean isSuccessful = userAccountC.createUserAccount(user);
-
-        if (isSuccessful) {
-            UserProfile userProfile = userProfileC.getProfileById(user.getProfileId());
-            String profileName = userProfile.getProfileName();  
-            
-            model.addAttribute("userAccountInfo", user);
-            model.addAttribute("profileName", profileName);
-            System.out.println("Signup successful");
-            return "user_account_info";
-        } else {
-            model.addAttribute("error", "Signup failed! Please try again.");
-            System.out.println("Signup failed");
-            return "cleaner_user_creation";
-        }
-    }
-
-    @GetMapping("/UpdateUserAccount")
-    public String updateUserAccount(@RequestParam String username, Model model) {
-        UserAccount user = userAccountC.getUserByUsername(username);
-        System.out.println(user.getProfileId());
-        model.addAttribute("updateUserAccountForm", user);
-        return "user_account_update";
-    }
-
-    @PostMapping("/UpdateUserAccount")
-    public String processUpdateUserAccount(@ModelAttribute UserAccount user, Model model) {
-        System.out.println(user.getUid());
-        boolean isSuccessful = userAccountC.updateUserAccount(user);
+        boolean isSuccessful = userAccountC.CreateAccount(user.getName(), user.getAge(), user.getDob(), user.getGender(), user.getAddress(), user.getEmail(), user.getUsername(),user.getPassword(),user.getProfileId()); 
 
         if (isSuccessful) {
             UserProfile userProfile = userProfileC.getProfileById(user.getProfileId());
             String profileName = userProfile.getProfileName();
-
+            
             model.addAttribute("userAccountInfo", user);
             model.addAttribute("profileName", profileName);
-            System.out.println("Signup successful");
             return "user_account_info";
         } else {
-            model.addAttribute("error", "Profile update failed! Please try again.");
-            System.out.println("Profile update failed");
-            return "user_account_update";
+            model.addAttribute("error", "Signup failed! Please try again.");
+            return "cleaner_user_creation";
         }
     }
 
-    @GetMapping("/UserAccountList")
+    //View User Account
+    @GetMapping("/ViewUserAccount")
+    public String showUserAccount(@RequestParam int uid, Model model) {
+        UserAccount user = userAccountC.ViewUserAccount(uid);
+        UserProfile userProfile = userProfileC.getProfileById(user.getProfileId());
+        String profileName = userProfile.getProfileName();
+
+        model.addAttribute("userAccountInfo", user);
+        model.addAttribute("profileName", profileName);
+        return "user_account_info";
+    }
+
+    @GetMapping("/ViewAllUserAccounts")
     public String showUserAccountList(Model model) {
-        List<UserAccount> userAccounts = userAccountC.getAllUsers();
+        List<UserAccount> userAccounts = userAccountC.GetAllUsers();
         List<String> profileNames = new ArrayList<>();
 
         for (UserAccount user : userAccounts) {
@@ -137,20 +113,41 @@ public class Boundary {
         return "user_account_list";
     }
 
-    @GetMapping("/UserAccount")
-    public String showUserAccount(@RequestParam int uid, Model model) {
-        UserAccount user = userAccountC.getUserById(uid);
-        UserProfile userProfile = userProfileC.getProfileById(user.getProfileId());
-        String profileName = userProfile.getProfileName();
-
-        model.addAttribute("userAccountInfo", user);
-        model.addAttribute("profileName", profileName);
-        return "user_account_info";
+    //Update User Account
+    @GetMapping("/UpdateUserAccount")
+    public String updateUserAccount(@RequestParam String username, Model model) {
+        UserAccount user = userAccountC.ViewUserAccount(username);
+        model.addAttribute("updateUserAccountForm", user);
+        return "user_account_update";
     }
 
+    @PostMapping("/UpdateUserAccount")
+    public String processUpdateUserAccount(@ModelAttribute UserAccount user, Model model) {
+        boolean isSuccessful = userAccountC.UpdateUserAccount(user.getName(), user.getAge(), user.getDob(), user.getGender(), user.getAddress(), user.getEmail(), user.getUsername(),user.getPassword(),user.getProfileId(), user.getUid()); 
+
+        if (isSuccessful) {
+            UserProfile userProfile = userProfileC.getProfileById(user.getProfileId());
+            String profileName = userProfile.getProfileName();
+
+            model.addAttribute("userAccountInfo", user);
+            model.addAttribute("profileName", profileName);
+            return "user_account_info";
+        } else {
+            model.addAttribute("error", "Profile update failed! Please try again.");
+            return "user_account_update";
+        }
+    }
+
+    //Suspend User Accounts
+    @PostMapping("/SuspendUserAccount")
+    public String suspendUserAccounts(@ModelAttribute UserAccount user, Model model) {
+        return "user_account_suspend";
+    }
+
+    //Search User Account
     @GetMapping("/searchUserAccount")
     public String searchUserAccounts(@RequestParam String query, Model model) {
-    List<UserAccount> userAccounts = userAccountC.searchUsersByUsername(query);
+    List<UserAccount> userAccounts = userAccountC.SearchUser(query);
     List<String> profileNames = new ArrayList<>();
 
     for (UserAccount user : userAccounts) {
@@ -164,6 +161,7 @@ public class Boundary {
     return "user_account_list";
     }
 
+    //Create User Profile
     @GetMapping("/CreateUserProfile")
     public String createUserProfile(Model model) {
         model.addAttribute("userProfileForm", new UserProfile());
@@ -172,7 +170,7 @@ public class Boundary {
 
     @PostMapping("/CreateUserProfile")
     public String processUserProfile(@ModelAttribute UserProfile userProfile, Model model) {
-        System.out.println(userProfile.isSuspended());
+        //System.out.println(userProfile.isSuspended());
         boolean isSuccessful = userProfileC.createUserProfile(userProfile);
 
         if (isSuccessful) {
@@ -185,7 +183,32 @@ public class Boundary {
             return "cleaner_user_creation";
         }
     }
+
+    //View User Profile
+    @GetMapping("/UserProfile")
+    public String showUserProfile(@RequestParam int profileId, Model model) {
+        UserProfile userProfile = userProfileC.getProfileById(profileId);
     
+        model.addAttribute("userProfileInfo", userProfile);
+        return "user_profile_info";
+    }
+
+    @GetMapping("/UserProfileList")
+    public String showUserProfileList(Model model) {
+        List<UserProfile> userProfiles = userProfileC.getAllProfiles();
+        List<Integer> profilesUserCounter = new ArrayList<>();
+
+        for (UserProfile userProfile : userProfiles) {
+            List<UserAccount> userAccounts = userAccountC.SearchUser(userProfile.getProfileId());
+            profilesUserCounter.add(userAccounts.size());
+        }
+
+        model.addAttribute("userProfiles", userProfiles);
+        model.addAttribute("profilesUserCounter", profilesUserCounter);
+        return "user_profile_list";
+    }
+    
+    //Update User Profile
     @GetMapping("/UpdateUserProfile")
     public String updateUserProfile(@RequestParam String profileName, Model model) {
         int profileId= userProfileC.getProfileIdByName(profileName);
@@ -209,36 +232,14 @@ public class Boundary {
         }
     }
 
-    @GetMapping("/UserProfileList")
-    public String showUserProfileList(Model model) {
-        List<UserProfile> userProfiles = userProfileC.getAllProfiles();
-        List<Integer> profilesUserCounter = new ArrayList<>();
-
-        for (UserProfile userProfile : userProfiles) {
-            List<UserAccount> userAccounts = userAccountC.searchUsersByProfileId(userProfile.getProfileId());
-            profilesUserCounter.add(userAccounts.size());
-        }
-
-        model.addAttribute("userProfiles", userProfiles);
-        model.addAttribute("profilesUserCounter", profilesUserCounter);
-        return "user_profile_list";
-    }
-
-    @GetMapping("/UserProfile")
-    public String showUserProfile(@RequestParam int profileId, Model model) {
-        UserProfile userProfile = userProfileC.getProfileById(profileId);
-    
-        model.addAttribute("userProfileInfo", userProfile);
-        return "user_profile_info";
-    }
-
+    //Search User Profile
     @GetMapping("/searchUserProfile")
     public String searchUserProfile(@RequestParam String query, Model model) {
     List<UserProfile> userProfiles = userProfileC.searchProfilesByName(query);
     List<Integer> profilesUserCounter = new ArrayList<>();
 
     for (UserProfile userProfile : userProfiles) {
-        List<UserAccount> userAccounts = userAccountC.searchUsersByProfileId(userProfile.getProfileId());
+        List<UserAccount> userAccounts = userAccountC.SearchUser(userProfile.getProfileId());
         profilesUserCounter.add(userAccounts.size());
     }
 
